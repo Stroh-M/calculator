a0f2bcb914e7ca6f32e6324fa33f4c7e46628577
Stroh-M <165234004+Stroh-M@users.noreply.github.com>
2025-01-29 15:22:59 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+function add(x, y) {
+    const total = x + y;
+    return total;
+  }
+
+  function subtract(x, y) {
+    const total = x - y;
+    return toLocalString(total);
+  }
+
+  function divide(x, y) {
+    return x / y;
+  }
+
+  function multiply(x, y) {
+    return x * y;
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    const x = parseInt(document.getElementById("x").value);
+    const y = parseInt(document.getElementById("y").value);
+
+    const calc = document.querySelector("select").value;
+    switch (calc) {
+      case "add":
+        document.querySelector("p.output").innerHTML = add(x, y);
+        break;
+      case "subtract":
+        document.querySelector("p.output").innerHTML = subtract(x, y);
+        break;
+      case "divide":
+        document.querySelector("p.output").innerHTML = divide(x, y);
+        break;
+      case "multiply":
+        document.querySelector("p.output").innerHTML = multiply(x, y);
+        break;
+      default:
+        break;
+    }
+  }
+
+  document.querySelector("button.reset").addEventListener("click", () => {
+    document.querySelector(".output").textContent = "";
+  });
